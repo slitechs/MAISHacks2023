481dccec421e8120831c8c30a56467f1655a0f99
Sophia <44929270+slitechs@users.noreply.github.com>
2023-09-30 20:22:06 -0400

    Added GET and POST between frontend and backend

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 # Import flask
-from flask import Flask, request, redirect, render_template # . .venv/bin/activate to activate virtual environment before running
+from flask import Flask, request, redirect, render_template, send_file # . .venv/bin/activate to activate virtual environment before running
+from flask_cors import CORS
 import datetime
 
 import os
@@ -12,6 +13,7 @@ ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 # Initializing flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins":"*"}})
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -24,27 +26,37 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return redirect(request.url)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return redirect(request.url)
 
-        file = request.files['file']
+    file = request.files['file']
 
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        else:
-            return "File type not supported."
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    else:
+        return "File type not supported."
 
-    return render_template('upload.html')
+    return "Uploaded an image."
 
+@app.route('/image', methods=['GET'])
+def get_image():
+    image_name = request.args.get('img') # Default: value None
+    if image_name is None:
+        return
+    
+    stored_images = os.listdir("./photos")
+    if image_name in stored_images:
+        return send_file(f"./photos/{image_name}", mimetype='image/png')
+    
+    return 
 
 # Route for seeing a data
 @app.route('/data')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,65 @@
 import React, {useState, useEffect, Component} from 'react';
 import './App.css';
 import { Button } from '@mui/material';
+import axios from "axios";
 
 function App() {
 
   const [file, setFile] = useState();
   const [fileUrl, setFileUrl] = useState(null);
-
+  
   function processImage(e) {
     const imageFile = e.target.files[0];
+    setFile(imageFile)
     const imageUrl = URL.createObjectURL(imageFile); //generates a temporary url for the selected file
     setFileUrl(imageUrl)
+  }
+
+  async function handleSave(e) {
+    console.log("save pressed")
+
+    const formData = new FormData();
+    formData.append("file", file) // file in backend
+
+    // POST request to send image to backend
+    var response = await fetch(
+      "http://127.0.0.1:5000/upload",
+      {
+        method: "post",
+        body: formData
+      }
+    )
+
+    if (response.status == 200) {
+      console.log("It worked")
+    }
+  }
+
+  async function getImage() {
+    var image_name = "Google_logo.png"
+    // GET request
+    var response = await fetch(
+      "http://127.0.0.1:5000/image?img="+image_name
+    )
+
+    if (response.status == 200) {
+      console.log("Get worked")
+    }
+
+    console.log(response)
+  }
+
+  // Axios POST (unused)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const imageData = {
+      url: fileUrl
+    }
+    axios.post("http://localhost:5000/upload", imageData).then((response) => {
+      console.log(response.status, response.data.token);
+    });
+
   }
 
   const [data,setData] = useState({
@@ -19,6 +68,8 @@ function App() {
     date: "",
     programming: "",
   });
+
+  
 
   useEffect(() => {
     // Using fetch to fetch the api from flask server it will be redirected to proxy
@@ -57,8 +108,17 @@ function App() {
         <input type="file" accept="image/*" onChange={processImage} />
       </label>
       </Button>
-      <Button>Save</Button>
+      <Button onClick={
+        handleSave
+      }>Save</Button>
       <img class="image-preview" src={fileUrl}/>
+
+      <Button variant="outlined" onClick={
+        getImage
+      }>Outfit Option 1</Button>
+      <Button variant="outlined" onClick={
+        getImage
+      }>Outfit Option 2</Button>
     </div>
       
     </div>
